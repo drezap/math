2b770857ce7e2c27de804aed1e5d20d359d59d41
andre <andre@cluster-local.node.drelen>
2018-07-16 11:35:14 +0300

    inital commit to mono

--- stan/math/prim/mat/fun/gp_exp_quad_mono_cov.hpp
+++ stan/math/prim/mat/fun/gp_exp_quad_mono_cov.hpp
@@ -1,9 +1,10 @@
 #ifndef STAN_MATH_PRIM_MAT_FUN_GP_EXP_QUAD_MONO_COV_HPP
 #define STAN_MATH_PRIM_MAT_FUN_GP_EXP_QUAD_MONO_COV_HPP
 
+#include <cmath>
 #include <math.h>
-#include <stan/math/prim/mat/fun/distance.hpp>
 #include <stan/math/prim/mat/fun/Eigen.hpp>
+#include <stan/math/prim/mat/fun/distance.hpp>
 #include <stan/math/prim/scal/err/check_not_nan.hpp>
 #include <stan/math/prim/scal/err/check_positive.hpp>
 #include <stan/math/prim/scal/err/check_size_match.hpp>
@@ -13,48 +14,45 @@
 #include <stan/math/prim/scal/fun/inv_square.hpp>
 #include <stan/math/prim/scal/fun/square.hpp>
 #include <stan/math/prim/scal/meta/return_type.hpp>
-#include <cmath>
 #include <vector>
 
 namespace stan {
 namespace math {
 
-  template<typename T_x, typename T_m, typename T_l>
+template <typename T_x, typename T_m, typename T_l>
 inline typename Eigen::Matrix<
     typename stan::return_type<T_x, T_sigma, T_l, T_p>::type, Eigen::Dynamic,
     Eigen::Dynamic>
-  gp_exp_quad_mono_cov(const std::vector<T_x> &x,
-                       const T_m &magnitude, const T_l &length_scale) {
-  
-    using std::exp;
-    // TODO check proper errors (domain/range)
+gp_exp_quad_mono_cov(const std::vector<T_x> &x, const T_m &magnitude,
+                     const T_l &length_scale) {
 
-    size_t x_size = x.size();
-    size_t x_size2 = 2 * x_size;
+  using std::exp;
+  // TODO check proper errors (domain/range)
 
-    //this will need to be piped into backward compatable
-    Eigen::Matrix<typename stan::return_type<T_x, T_sigma, T_l, T_p>::type,
-                  Eigen::Dynamic, Eigen::Dynamic>
-      exp_quad_cov = cov_exp_quad
-    
-    Eigen::Matrix<typename stan::return_type<T_x, T_sigma, T_l, T_p>::type,
+  size_t x_size = x.size();
+  size_t x_size2 = 2 * x_size;
+
+  // this will need to be piped into backward compatable
+  Eigen::Matrix<typename stan::return_type<T_x, T_sigma, T_l, T_p>::type,
                 Eigen::Dynamic, Eigen::Dynamic>
-      cov(x_size2, x_size2);
+      exp_quad_cov = cov_exp_quad
 
-    T_m mag_sq = square(magnitude);
-    
-    for (int i = 0; i < x_size2; ++i) {
-      for (int j = 0; j < x_size3; ++j) {
-        
-      }
+          Eigen::Matrix<
+              typename stan::return_type<T_x, T_sigma, T_l, T_p>::type,
+              Eigen::Dynamic, Eigen::Dynamic>
+              cov(x_size2, x_size2);
+
+  T_m mag_sq = square(magnitude);
+
+  for (int i = 0; i < x_size2; ++i) {
+    for (int j = 0; j < x_size3; ++j) {
     }
-    
-    
+  }
 
   return cov;
 }
 
-}  // namespace math
-}  // namespace stan
+} // namespace math
+} // namespace stan
 
 #endif
