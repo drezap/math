08c02f4ab8e8b17f2051edcd73de5954d7fd1484
andre <andre@cluster-local.node.drelen>
2019-06-19 16:04:58 -0400

    add bernoulli std::vector, row_vector and scalar tetests

--- test/unit/math/prim/mat/prob/bernoulli_logit_log_test.cpp
+++ test/unit/math/prim/mat/prob/bernoulli_logit_log_test.cpp
@@ -3,6 +3,7 @@
 
 using Eigen::Dynamic;
 using Eigen::Matrix;
+using Eigen::Array;
 
 TEST(ProbBernoulliLogit, log_matches_lpmf) {
   Matrix<int, Dynamic, 1> n(3, 1);
@@ -29,4 +30,21 @@ TEST(ProbBernoulliLogit, log_matches_lpmf) {
   EXPECT_FLOAT_EQ(
       (stan::math::bernoulli_logit_lpmf<Matrix<int, Dynamic, 1>>(n, theta)),
       (stan::math::bernoulli_logit_log<Matrix<int, Dynamic, 1>>(n, theta)));
+}
+
+
+TEST(ProbBernoulliLogit, test_different_types) {
+  std::vector<double> vec_test(3);
+  vec_test[0] = 0; vec_test[1] = 1; vec_test[2] = 2;
+  Matrix<int, Dynamic, 1> n(3, 1);
+  n << 0, 1, 0;
+  EXPECT_NO_THROW(stan::math::bernoulli_logit_lpmf(n, vec_test));
+
+  double scalar_test = 1.0;
+  int n1 = 1.0;
+  EXPECT_NO_THROW(stan::math::bernoulli_logit_lpmf(n1, scalar_test));
+
+  Matrix<double, 1, -1> row_vec(3);
+  row_vec << 1, 2, 3;
+  EXPECT_NO_THROW(stan::math::bernoulli_logit_lpmf(n, row_vec));
 }
