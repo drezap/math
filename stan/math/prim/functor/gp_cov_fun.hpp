24af035cbfc7bbb9da6b6a0fbdb4fade9c69cb0d
andre <andre@cluster-local.node.drelen>
2019-07-03 15:40:00 -0400

    computed alpha

--- stan/math/prim/functor/gp_cov_fun.hpp
+++ stan/math/prim/functor/gp_cov_fun.hpp
@@ -5,7 +5,11 @@
 #include <stan/math/prim/mat/fun/Eigen.hpp>
 #include <stan/math/prim/scal/meta/return_type.hpp>
 #include <stan/math/prim/scal/meta/partials_return_type.hpp>
+#include <stan/math/prim/mat/fun/cholesky_decompose.hpp>
+#include <stan/math/prim/mat/fun/inverse.hpp>
 #include <iostream>
+#include <type_traits>
+#include <Eigen/Dense>
 
 namespace stan {
 namespace math {
@@ -14,14 +18,30 @@ template <bool propto, typename F, typename T_y, typename T_X, typename T_theta>
 class gp_lpdf {
 public:
   const F& f_;
-  const std::vector<T_X>& X_;
+  const Eigen::Matrix<T_X, Eigen::Dynamic, Eigen::Dynamic>& X_;
+   const Eigen::Matrix<T_y, Eigen::Dynamic, 1> & y;
+  //  const std::vector<T_theta>& theta;
   Eigen::Matrix<typename partials_return_type<T_y, T_X, T_theta>::type,
                   Eigen::Dynamic, Eigen::Dynamic> C_dbl_;
   gp_lpdf(const F& f_,
-          const std::vector<T_X>& X_):
-    f_(f_), X_(X_)
+          const Eigen::Matrix<T_X, Eigen::Dynamic, Eigen::Dynamic>& X_,
+          Eigen::Matrix<T_y, Eigen::Dynamic, 1> y):
+    f_(f_),
+    X_(X_),
+    y(y)
+    //    theta(theta)
   {
-    C_dbl_ = f_(X_, 1.0, 1.0);
+    C_dbl_ = f_(X_, 1.0, 1.0);  // make this intake an arbitrary number of parameters
+    Eigen::Matrix<typename partials_return_type<T_y, T_X, T_theta>::type,
+                  Eigen::Dynamic, Eigen::Dynamic>
+    C_chol_ = cholesky_decompose(C_dbl_);  // Do we want to save the covariance matrix?
+
+    //    alpha = K^(-1) y
+    Eigen::Matrix<typename partials_return_type<T_y, T_X, T_theta>::type,
+                  Eigen::Dynamic, Eigen::Dynamic>
+    C_inv_ = inverse(C_dbl_);
+    Eigen::Matrix<typename return_type<T_y, T_X, T_theta>::type, Eigen::Dynamic, Eigen::Dynamic>
+      alpha = C_inv_ * y;
   };
 };
   
