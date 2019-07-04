9e5794f469988dd61a8e4b0fa1b9f4c901733171
andre <andre@cluster-local.node.drelen>
2019-07-03 20:21:05 -0400

    it compiles

--- stan/math/prim/functor/gp_cov_fun.hpp
+++ stan/math/prim/functor/gp_cov_fun.hpp
@@ -17,22 +17,23 @@ namespace math {
 template <bool propto, typename F, typename T_y, typename T_X, typename T_theta>
 class gp_lpdf {
 public:
-  const F& f_(Eigen::Matrix<T_X, Eigen::Dynamic, Eigen::Dynamic>, T_theta...);
+  const F& f_(const Eigen::Matrix<T_X, Eigen::Dynamic, Eigen::Dynamic>, T_theta...);
   const Eigen::Matrix<T_X, Eigen::Dynamic, Eigen::Dynamic>& X_;
   const Eigen::Matrix<T_y, Eigen::Dynamic, 1> & y;
   const std::vector<T_theta>& theta;
   
   Eigen::Matrix<typename partials_return_type<T_y, T_X, T_theta>::type,
                   Eigen::Dynamic, Eigen::Dynamic> C_dbl_;
-  gp_lpdf()//const F& f_):
-    //       const Eigen::Matrix<T_X, Eigen::Dynamic, Eigen::Dynamic>& X_,
+  gp_lpdf(const F& f_(const Eigen::Matrix<T_X, Eigen::Dynamic, Eigen::Dynamic>, T_theta...))//,//:
+          //          const Eigen::Matrix<T_X, Eigen::Dynamic, Eigen::Dynamic>& X_)//,
     //       Eigen::Matrix<T_y, Eigen::Dynamic, 1> y):
-  //    f_(f_)
+    //    f_(f_(Eigen::Matrix<T_X, Eigen::Dynamic, Eigen::Dynamic>, T_theta...))
     // X_(X_),
     // y(y),
     // theta(theta)
   {
-    // C_dbl_ = f_(X_, 1.0, 1.0);
+    //    C_dbl_ = f_(X_, theta);
+    //    std::cout << C_dbl_ << "\n";
     // Eigen::Matrix<typename partials_return_type<T_y, T_X, T_theta>::type,
     //               Eigen::Dynamic, Eigen::Dynamic>
     //   C_chol_ = cholesky_decompose(C_dbl_);
