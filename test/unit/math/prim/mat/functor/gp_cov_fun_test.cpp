0287f5ae0a4aa9b17f95765bed038b264b6c42f9
andre <andre@cluster-local.node.drelen>
2019-07-10 17:27:24 -0400

    change functor input data

--- test/unit/math/prim/mat/functor/gp_cov_fun_test.cpp
+++ test/unit/math/prim/mat/functor/gp_cov_fun_test.cpp
@@ -9,25 +9,28 @@ struct cov_exp_quad_functor {
     template <typename T0__, typename T1__, typename T2__>
         Eigen::Matrix<typename boost::math::tools::promote_args<T0__, T1__, T2__>::type,
                       Eigen::Dynamic,Eigen::Dynamic>
-    operator()(const std::vector<T0__>& x,
+    operator()(const Eigen::Matrix<T0__, -1, -1>& x,
                const T1__& sigma,
                const T2__& length_scale) const {
-               //               ,std::ostream* pstream__) const {
       typedef typename boost::math::tools::promote_args<T0__, T1__>::type scalar;
+      std::vector<Eigen::Matrix<T0__, -1, 1>> x_in(x.cols());
+      for (size_t i = 0; i < x.cols(); ++i) {
+        x_in.resize(x.rows(), 1);
+        x_in[i] = x.col(i);
+      }
       Eigen::Matrix<scalar, -1, -1> cov =
-        stan::math::gp_exp_quad_cov(x, sigma, length_scale);
+        stan::math::gp_exp_quad_cov(x_in, sigma, length_scale);
       return cov;
     }
 };
 
 TEST(MathPrimFunctor, test_compute_cov) {
-  std::vector<double> x(3); // only use column vectors
-  x[0] = -2;
-  x[1] = -1;
-  x[2] = -0.5;
+  Eigen::Matrix<double, -1, -1> x; x.resize(3, 3);
+  x << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+  
   stan::math::gp_lpdf <false, cov_exp_quad_functor,
                        double, double, stan::math::var>
-    gp_lpdf(cov_exp_quad_functor(), x);
+    gp_lpdf(cov_exp_quad_functor());
   //  stan::math::gp_lpdf(L_cov_exp_quad_functor__());
   
 }
